09319ee774d0520b3c33c94270ae7e1cd8a4665b
p410n3 <p410n3@gmail.com>
2020-05-18 18:13:15 +0200

    Get most SPA logic working + somewhat nice CSS

--- script.js
+++ script.js
@@ -6,51 +6,73 @@ const pages = document.getElementById('pages');
 
 const apiUrl = 'https://wp.palone.blog/?rest_route=';
 
-// Get pages
-fetch(apiUrl + '/wp/v2/pages&_fields=title,id,slug')
-    .then(res => res.json())
-    .then(data => {
-        data.map(page => {
-            let el = document.createElement('a');
-            el.href = '#page-' + page.slug;
-            el.innerText = page.title.rendered;
+// Render links once DOM is loaded
+window.addEventListener('load', () => {
+    renderLinks();
+    if(window.location.hash) renderContent(); // remderContent on pageload if a hash is set
+});
 
-            el.addEventListener('click', () => {
-                fetch(apiUrl + '/wp/v2/pages/' + page.id + '&_fields=content')
-                    .then(res => res.json())
-                    .then(data => {
-                        renderModal(data.content.rendered);
-                    });
-            });
-
-            pages.appendChild(el);
-        });
-    });
-
-// Get posts
-fetch(apiUrl + '/wp/v2/posts&_fields=title,id,slug,date,excerpt')
-    .then(res => res.json())
-    .then(data => {
-        data.map(post => {
-            let el = document.createElement('a');
-            el.href = '#post-' + post.slug;
-            el.innerHTML = post.title.rendered + post.excerpt.rendered + post.date; // TODO render the stuff actually neatly
-
-            el.addEventListener('click', () => {
-                fetch(apiUrl + '/wp/v2/posts/' + post.id + '&_fields=content')
-                    .then(res => res.json())
-                    .then(data => {
-                        renderModal(data.content.rendered);
-                    });
-            });
-
-            posts.appendChild(el);
-        });
-    });
-
+// Show content in modal when the location hash changes
+window.addEventListener('hashchange', () => {
+    renderContent();
+});
 
 // Functions down here
-function renderModal(content) {
-    modal.innerHTML = content;
-    modal.classList.add('show');
+function renderLinks() {
+    // Get pages
+    fetch(`${apiUrl}/wp/v2/pages&_fields=title,id,slug`)
+        .then(res => res.json())
+        .then(data => {
+            data.map(page => {
+                let el = `
+                    <a href="#${page.slug}-${page.id}">${page.title.rendered}</a>
+                `;
+
+                pages.innerHTML += el;
+            });
+        });
+
+    // Get posts
+    fetch(`${apiUrl}/wp/v2/posts&_fields=title,id,slug,excerpt`)
+        .then(res => res.json())
+        .then(data => {
+            data.map(post => {
+                let el = `
+                    <div class="post">
+                        <h2>
+                            <a href="#post-${post.slug}-${post.id}">${post.title.rendered}</a>
+                        </h2>
+                        <span>
+                            ${post.excerpt.rendered}
+                        </span>
+                        <a href="#post-${post.slug}-${post.id}">Read More</a>
+                    </div>
+                `;
+                posts.innerHTML += el;
+            });
+        });
+}
+
+function renderContent() {
+    // If there is no location hash, close modal and retirn early
+    if(!window.location.hash) {
+        modal.classList.remove('show');
+        return;
+    }
+
+    let type = 'pages'; // Assume page by default
+    if(window.location.hash.match(/^#post/)) type = 'posts'; // Change to post if needed
+
+    let id = window.location.hash.match(/[0-9]+$/)[0];
+
+    fetch(`${apiUrl}/wp/v2/${type}/${id}&_fields=content, title`)
+        .then(res => res.json())
+        .then(data => {
+            modal.innerHTML = `
+                <a href="#" class="back">back</a>
+                <h1>${data.title.rendered}</h1>
+                ${data.content.rendered}
+            `;
+            modal.classList.add('show');
+        });
 }
